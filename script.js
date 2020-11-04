290bab65cc6a0976dc6271839867d4ade808c423
natia.gelashvili <natia.gelashvili@flatrocktech.com>
2020-11-04 10:23:32 +0400

    init

---
+++ script.js
@@ -0,0 +1,17 @@
+
+        //  012345678910
+var data = "Hello,World,Data,Data2";
+
+// console.log( data.slice(6) );
+// console.log( data.slice(6, 11) );
+// console.log( data.slice(-3) );
+
+// console.log( data.substr(6, 5) );
+
+// console.log( data.toUpperCase() );
+// console.log( data.toLowerCase() );
+
+// console.log( data.concat(" Data", " Data2") );
+// console.log( data + " Data Data2" );
+
+// console.log( data.split("") );
